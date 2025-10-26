19b9c794eb7d675038db4c2072f3ab6983c9c607
proptz06-cloud <proptz06@gmail.com>
2025-10-26 19:00:36 +0530

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Link from 'next/link';
+import HomePage from './(marketing)/Home/page';
 
 export default function App() {
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen gap-4'>
-      <h1 className='text-3xl font-bold'>Welcome to Proptz</h1>
-      <Link href='/Home' className='text-blue-600 hover:underline'>
-        Go to Marketing Home
-      </Link>
-    </div>
+    <>
+      <HomePage />
+    </>
   );
 }
