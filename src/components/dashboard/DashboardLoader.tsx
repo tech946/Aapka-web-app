7763b59f846b7cdb4565bb8bfa16544c15290417
Aapka Tourism <aapkatourism@users.noreply.github.com>
2026-02-26 01:53:09 +0530

    aa2

--- src/components/dashboard/DashboardLoader.tsx
+++ src/components/dashboard/DashboardLoader.tsx
@@ -2,8 +2,8 @@
 
 export function DashboardLoader() {
   return (
-    <div className="dashboard_loader">
-      <div className="dashboard_loader_spinner" />
+    <div className='dashboard_loader'>
+      <div className='dashboard_loader_spinner' />
       <p>Loading dashboard...</p>
     </div>
   );
